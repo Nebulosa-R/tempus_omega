de1c368b7c761e377b0fa4e112c8880c5d4c8281
Laura <laura.y.lp96@gmail.com>
2019-09-08 16:43:57 -0300

    .

--- www/static/js/index.js
+++ www/static/js/index.js
@@ -10,7 +10,50 @@ $(document).ready(function(){
     });
 
 	$('#alternar').click(function(){
-		$('#prueba').toggle();
+		$('#prueba').toggle(
+
+			// Primer click
+	        function(e){ 
+	            $('#alternar').slideDown();
+	            $(this).text('Ocultar respuesta');
+	            e.preventDefault();
+	        }, // Separamos las dos funciones con una coma
+	      
+	        // Segundo click
+	        function(e){ 
+	            $('#alternar').slideUp();
+	            $(this).text('Ver respuesta');
+	            e.preventDefault();
+	        }
+
+		);
 	});
+
+
+	 $('#alternar-panel-oculto').toggle( 
+ 
+        /* 
+            Primer click.
+            Función que descubre un panel oculto
+            y cambia el texto del botón.
+        */
+        function(e){ 
+            $('#panel-oculto').slideDown();
+            $(this).text('Cerrar el panel');
+            e.preventDefault();
+        }, // Separamos las dos funciones con una coma
+     
+        /* 
+            Segundo click.
+            Función que oculta el panel
+            y vuelve a cambiar el texto del botón.
+        */
+        function(e){ 
+            $('#panel-oculto').slideUp();
+            $(this).text('Mostrar el panel oculto');
+            e.preventDefault();
+        }
+ 
+    );
 
 });
