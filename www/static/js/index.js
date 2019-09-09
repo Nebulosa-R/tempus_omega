1d730bded669a5c37ec32d0d9848ace5df7aa140
Laura <laura.y.lp96@gmail.com>
2019-09-09 19:30:31 -0300

    .

--- www/static/js/index.js
+++ www/static/js/index.js
@@ -1,87 +1,19 @@
 $(document).ready(function(){
-   /*
-    $("#hide").on('click', function() {
-            $("#element").hide();
-            return false;
-    });
- 
-    $("#show").on('click', function() {
-        $("#element").show();
-        return false;
-    });
 
-	$('#alternar').click(function(){
-		$('#prueba').toggle(
-
-			// Primer click
-	        function(e){ 
-	            $('#alternar').slideDown();
-	            $(this).text('Ocultar respuesta');
-	            e.preventDefault();
-	        }, // Separamos las dos funciones con una coma
-	      
-	        // Segundo click
-	        function(e){ 
-	            $('#alternar').slideUp();
-	            $(this).text('Ver respuesta');
-	            e.preventDefault();
-	        }
-
-		);
+	/*$('#cambio_texto').click(function() {
+		valor =  $("#boton").attr("value");
+	 	if ( valor.equals("Mostrar canciones") ) {
+	    	//$(this).text( "Ocultar canciones" );
+	    	 $("#boton").attr("value","Ocultar canciones");
+	  	}else {
+	    	$("#boton").attr("value","Mostrar canciones");
+  		}
+	}); 
+*/
+	$('#cambio_texto').click(function() {
+		if ($(this).html() == "Mostrar canciones")
+			$(this).html("Ocultar canciones");
+		else
+			$(this).html("Mostrar canciones")
 	});
-	*/
-
-	 $('#alternar-panel-oculto').toggle( 
- 
-        /* 
-            Primer click.
-            Función que descubre un panel oculto
-            y cambia el texto del botón.
-        */
-        function(e){ 
-            $('#panel-oculto').slideDown();
-            $(this).text('Cerrar el panel');
-            e.preventDefault();
-        }, // Separamos las dos funciones con una coma
-     
-        /* 
-            Segundo click.
-            Función que oculta el panel
-            y vuelve a cambiar el texto del botón.
-        */
-        function(e){ 
-            $('#panel-oculto').slideUp();
-            $(this).text('Mostrar el panel oculto');
-            e.preventDefault();
-        }
- 
-    );
-
-
-	 $('#alternar-panel-oculto').toggle( 
- 
-        /* 
-            Primer click.
-            Función que descubre un panel oculto
-            y cambia el texto del botón.
-        */
-        function(e){ 
-            $('#panel-oculto').slideDown();
-            $(this).text('Cerrar el panel');
-            e.preventDefault();
-        }, // Separamos las dos funciones con una coma
-     
-        /* 
-            Segundo click.
-            Función que oculta el panel
-            y vuelve a cambiar el texto del botón.
-        */
-        function(e){ 
-            $('#panel-oculto').slideUp();
-            $(this).text('Mostrar el panel oculto');
-            e.preventDefault();
-        }
- 
-    );
-
 });
